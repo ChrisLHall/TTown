6b5e7e63d7ddb12f710c1c6d6b68ef76d40a9f3e
ChrisLHall <mrchrislhall@gmail.com>
2018-05-17 12:25:57 -0400

    Animals spawn and move around with the Simulate button

--- ttownbot.js
+++ ttownbot.js
@@ -1,4 +1,7 @@
 function listRand(list) { return list[Math.floor(Math.random() * list.length)] }
+function clamp(num, min, max) {
+  return Math.min(Math.max(num, min), max);
+};
 
 var WIDTH = 13
 var HEIGHT = 9
@@ -7,7 +10,7 @@ function initMap() {
   var map = []
   for (var row = 0; row < HEIGHT; row++) {
     var rowList = []
-    for (var col = 0; col < HEIGHT; col++) {
+    for (var col = 0; col < WIDTH; col++) {
       var rand = Math.random()
       var tile = "."
       if (rand < .1) {
@@ -28,14 +31,17 @@ function generateLake(map) {
 
 function spawnAnimals(objects, num) {
   for (var j = 0; j < num; j++) {
+    console.log("spawning " + j)
     var type = listRand(Animal.spawnTypes)
-    var animal = new Animal(Math.floor(Math.random() * WIDTH), Math.floor(Math.random() * HEIGHT, type)
+    var animal = new Animal(Math.floor(Math.random() * WIDTH), Math.floor(Math.random() * HEIGHT), type)
     objects.push(animal)
   }
 }
 
-function simulate() {
-
+function simulate(objects) {
+  for (var o = 0; o < objects.length; o++) {
+    objects[o].simulate()
+  }
 }
 
 var EMOJI_SUBS = {
@@ -45,19 +51,40 @@ var EMOJI_SUBS = {
 }
 
 function render(isEmoji, map, objects) {
-  // todn make a temp map first
-  var out = ""
+  console.log("starting render")
+  var outMap = []
   for (var row = 0; row < map.length; row++) {
     var rowList = map[row]
+    var outMapRow = []
     for (var col = 0; col < rowList.length; col++) {
       var tile = rowList[col]
-      out += (isEmoji ? EMOJI_SUBS[tile] : tile)
+      outMapRow.push(isEmoji ? EMOJI_SUBS[tile] : tile);
+    }
+    outMap.push(outMapRow)
+  }
+  
+  for (var o = 0; o < objects.length; o++) {
+    var obj = objects[o]
+    console.log(obj.type)
+    if (obj.y >= 0 && obj.y < map.length) {
+      var outMapRow = outMap[obj.y]
+      if (obj.x >= 0 && obj.x < outMapRow.length) {
+        outMapRow[obj.x] = isEmoji ? obj.info.emoji : obj.info.text
+      }
+    }
+  }
+  
+  console.log("ready to render outmap")
+  var out = ""
+  for (var row = 0; row < outMap.length; row++) {
+    var outMapRow = outMap[row]
+    for (var c = 0; c < outMapRow.length; c++) {
+      out += outMapRow[c]
     }
     out += "\n"
   }
-  
-  //todo objects
-  
+  console.log("done")
+  console.log("map: " + out)
   return out
 }
 
@@ -69,7 +96,12 @@ var Animal = function(x, y, type) {
 }
 
 Animal.prototype.simulate = function() {
-  // todo
+  if (Math.random() < .3) {
+    var dx = -this.info.speed + Math.floor(Math.random() * (2 * this.info.speed + 1))
+    var dy = -this.info.speed + Math.floor(Math.random() * (2 * this.info.speed + 1))
+    this.x = clamp(this.x + dx, 0, WIDTH - 1)
+    this.y = clamp(this.y + dy, 0, HEIGHT - 1)
+  }
 }
 
 Animal.types = {
