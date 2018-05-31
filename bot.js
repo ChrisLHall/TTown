654c6b745673659b877ed46d0b9c33bdd34b0498
ChrisLHall <c3doggy@gmail.com>
2018-05-30 19:30:46 -0700

    Almost done with vote counting

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ var kii = require("kii-cloud-sdk").create()
 var KiiServerCreds = require('./KiiServerCreds')()
 
 var sim = new Simulation()
+var tweeter = new Twit(TwitterCreds)
 console.log("sim made successfully")
 
 var clippedArgs = process.argv.slice(2);
@@ -39,8 +40,58 @@ function onLoginSuccessful() {
   }
 }
 
-var simulated = false
 function onLoadFinished() {
+  if (sim.lastTweetID && sim.askedQuestion) {
+    console.log("Scanning for answers to last question")
+    searchForReplies(sim.lastTweetID, sim.askedQuestion, startSimulation)
+  } else {
+    startSimulation()
+  }
+}
+
+function searchForReplies(lastTweetID, askedQuestion, onSearchComplete) {
+  tweeter.get('search/tweets', { q: 'to:TinyTownSim', since_id: lastTweetID, count: 100 }, function(err, data, response) {
+    if (data.statuses) {
+      console.log("Total replies: " + data.statuses.length)
+      var directReplies = 0
+      var countedVotes = {}
+      for (var option = 0; option < askedQuestion.options.length; option++) {
+          countedVotes[askedQuestion.options[option].keyword] = []
+      }
+      for (var j = 0; j < data.statuses.length; j++) {
+        var status = data.statuses[j]
+        var userID = status.user.id
+        if (status.in_reply_to_status_id === lastTweetID) {
+          for (var key in countedVotes) {
+            if (countedVotes.hasOwnProperty(key)) {
+              // make sure they said the keyword
+              if (status.text.includes(key)) {
+                var votesList = countedVotes[key]
+                if (votesList.indexOf(userID) === -1) {
+                  // only add users who did not already vote
+                  votesList.push(userID)
+                }
+                break
+              }
+            }
+          }
+          directReplies++
+        }
+      }
+      console.log("Direct replies to this status: " + directReplies)
+      for (var key in countedVotes) {
+        if (countedVotes.hasOwnProperty(key)) {
+          var totalVotes = countedVotes[key].length
+          console.log(key + " got votes: " + totalVotes)
+        }
+      }
+    }
+    onSearchComplete()
+  })
+}
+
+var simulated = false
+function startSimulation() {
   simulated = false
   console.log("Sim time of day: " + sim.timeOfDay)
   var date = new Date()
@@ -71,7 +122,6 @@ function onLoadFinished() {
 }
 
 function performTweet() {
-  var tweeter = new Twit(TwitterCreds)
   tweeter.post('statuses/update', { status: sim.render(true) }, function(err, data, response) {
     console.log("Tweet posted")
     console.log(data)
